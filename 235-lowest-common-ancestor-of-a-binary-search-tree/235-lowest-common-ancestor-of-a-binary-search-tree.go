af6159eb925387dae1d9979fec574e165bc2aea1
Neville <31406314+aphilas@users.noreply.github.com>
2022-06-30 17:24:57 +0300

    Time: 31 ms (33.84%), Space: 8 MB (5.64%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.go
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.go
@@ -8,13 +8,11 @@
  */
 
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-	for {
-		if p.Val < root.Val && q.Val < root.Val {
-			root = root.Left
-		} else if p.Val > root.Val && q.Val > root.Val {
-			root = root.Right
-		} else {
-			return root
-		}
+    if p.Val < root.Val && q.Val < root.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	} else if p.Val > root.Val && q.Val > root.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	} else {
+		return root
 	}
 }
